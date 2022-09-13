8c87914773e30635920fbdc431a14bbce4cc9938
recvizit <paxmagelanic1@gmail.com>
2022-09-13 18:24:13 +0300

    final

--- api/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from blog.models import Post
-
-
-class PostSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('__all__')
--- api/viewsets.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .serializer import PostSerializers
-from blog.models import Post
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    name = Post
-    queryset = Post.objects.all()
-    serializer_class = PostSerializers
